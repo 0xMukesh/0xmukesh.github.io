5c380af136711aa6bd911ae2074a386ad7508712
Mukesh <hey.mukesh272921@gmail.com>
2026-06-06 03:09:42 +0530

    feat: add alpha conversion related section

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 function appendFooter() {
   const footer = document.createElement("footer");
   footer.innerHTML = `
-    <p>© ${new Date().getFullYear()} Mukesh · This website was inspired from <a href="https://latex.vercel.app">LaTeX.css</a> · <a href="mailto:mukeshreddy.work@gmail.com">mukeshreddy.work@gmail.com</a></p>
+    <p>© ${new Date().getFullYear()} Mukesh · This website was inspired from <a href="../vendor/latex-css">LaTeX.css</a> · <a href="mailto:mukeshreddy.work@gmail.com">mukeshreddy.work@gmail.com</a></p>
   `;
 
   document.body.appendChild(footer);
