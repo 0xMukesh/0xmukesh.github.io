ed9036ce8ab8be793af7171389c0b0011a8a0e31
Mukesh <mukeshreddy.work@gmail.com>
2026-08-21 16:28:57 +0530

    badges: add 0xc84 badge

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 function appendFooter() {
   const footer = document.createElement("footer");
   footer.innerHTML = `
-    <p>© ${new Date().getFullYear()} Mukesh · This website uses <a href="../vendor/latex-css">LaTeX.css</a> · <a href="mailto:mukeshreddy.work@gmail.com">mukeshreddy.work@gmail.com</a></p>
+    <p>© ${new Date().getFullYear()} Mukesh · This website uses <a href="../vendor/latex-css">LaTeX.css</a> · <a href="mailto:mukesh@0xc84.fyi">mukesh@0xc84.fyi</a></p>
   `;
 
   document.body.appendChild(footer);
